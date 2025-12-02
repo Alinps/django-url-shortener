e98d89c2526a2140fadb16306460c7469bf23ce7
alin <alinps8406@gmail.com>
2025-12-02 23:21:56 +0530

    created signup functionality

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import SignUp
 # Create your views here.
+def register(request):
+    if request.method=='POST':
+        form=SignUp(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration Successfull")
+            return render(request,'login.html')
+    else:
+        form=SignUp()
+    return render(request,'signup.html',{"forms":form})
