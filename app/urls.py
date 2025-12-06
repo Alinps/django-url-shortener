784397f51adffaf93b3492a6328eb97b8ba501c0
alin <alinps8406@gmail.com>
2025-12-06 17:14:13 +0530

    added logout functionality

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from app import views
 urlpatterns = [
     
     path('',views.register),
-    path("login/",views.login_user,name='login')
+    path("login/",views.login_user,name='login'),
+    path('logout/',views.logout_user,name='logout')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout
 from .forms import SignUp,login_form
 # Create your views here.
 def register(request):
@@ -20,9 +20,21 @@ def login_user(request):
         form=login_form(data=request.POST)
         if form.is_valid():
             user = form.get_user()
+            print(user)
             login(request,user)
+            print(request.POST)
             return render(request,'home.html',{'user':user})
     else:
         form=login_form()
     return render(request,'login.html',{'form':form})
 
+
+@login_required(login_url='/login/')
+def logout_user(request):
+    if request.method=='POST':
+        logout(request)
+        return redirect('login')
+    context={
+        'user':request.user
+    }
+    return render(request,'logout.html',{'context':context})
