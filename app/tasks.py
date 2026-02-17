059338d185acef871b6dba25cf36d9ed63e2e196
alin <alinps8406@gmail.com>
2026-02-18 00:55:49 +0530

    implemented adding orphaned rename key to be considered if system crashes and restarted, implemented lock for worker

--- app/tasks.py
+++ app/tasks.py
@@ -18,41 +18,86 @@ def enqueue_click(short_url_id, user_agent, device_type):
     )
 
 
+def handle_click_count(redis_conn, key):
+    if isinstance(key, bytes):
+        key = key.decode()
+
+    key_str = key
+    short_url_id = key_str.split(":")[-1]
+
+    count = int(redis_conn.get(key) or 0)
+
+    if count > 0:
+        ShortURLMeta.objects.filter(
+            short_url_id=short_url_id
+        ).update(
+            click_count=F("click_count") + count
+        )
+
+    redis_conn.delete(key)
+
+
+
+def handle_click_events(redis_conn, key):
+    if isinstance(key, bytes):
+        key = key.decode()
+    key_str = key
+
+    short_url_id = key_str.split(":")[-1]
+
+    events = redis_conn.lrange(key, 0, -1)
+
+    click_objects = []
+
+    for event in events:
+        decoded = event.decode()
+        user_agent, device_type = decoded.split("|")
+        click_objects.append(
+            ClickEvent(
+                short_url_id=short_url_id,
+                user_agent=user_agent,
+                device_type=device_type
+            )
+        )
+
+    if click_objects:
+        ClickEvent.objects.bulk_create(click_objects)
+
+    redis_conn.delete(key)
+
+
+
+
 @shared_task
 def flush_analytics():
     redis_conn = get_redis_connection("default")
 
-    for key in redis_conn.scan_iter("click_count:*"):
-        key_str = key.decode()
-        short_url_id = key_str.split(":")[1]
-        count = int(redis_conn.get(key))
+    lock_key = "analytics_flush_lock"
 
-        if count > 0:
-            ShortURLMeta.objects.filter(
-                short_url_id=short_url_id,
-            ).update(click_count=F("click_count")+count)
+    lock_acquired = redis_conn.set(lock_key, "1", nx=True, ex=60)
 
-        redis_conn.delete(key)
+    if not lock_acquired:
+        print("Flush already running.")
+        return
 
-    for key in redis_conn.scan_iter("click_events:*"):
-        key_str = key.decode()
-        short_url_id = key_str.split(":")[1]
-        events = redis_conn.lrange(key,0,-1)
+    try:
+        # First handle orphan processing keys
+        for key in redis_conn.scan_iter("processing:click_count:*"):
+            handle_click_count(redis_conn, key)
 
-        click_objects = []
+        for key in redis_conn.scan_iter("processing:click_events:*"):
+            handle_click_events(redis_conn, key)
 
-        for event in events:
-            decode = event.decode()
-            user_agent,device_type = decode.split("|")
-            click_objects.append(
-                ClickEvent(
-                    short_url_id=short_url_id,
-                    user_agent=user_agent,
-                    device_type=device_type
-                )
-            )
+        # Then handle fresh keys
+        for key in redis_conn.scan_iter("click_count:*"):
+            processing_key = f"processing:{key.decode()}"
+            redis_conn.rename(key, processing_key)
+            handle_click_count(redis_conn, processing_key)
 
-        if click_objects:
-            ClickEvent.objects.bulk_create(click_objects)
+        for key in redis_conn.scan_iter("click_events:*"):
+            processing_key = f"processing:{key.decode()}"
+            redis_conn.rename(key, processing_key)
+            handle_click_events(redis_conn, processing_key)
 
-        redis_conn.delete(key)
+    finally:
+        redis_conn.delete(lock_key)
