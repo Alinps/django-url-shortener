1a2c69d71fbddae9101ee7509778cfeeb3c59cc7
alin <alinps8406@gmail.com>
2026-03-05 23:50:42 +0530

    Changing the html template to base layout and partial,currently chnaged landingpage, login, signup and forgotpassword

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -95,7 +95,7 @@ PASSWORD_RESET_TIMEOUT = 86400  # 24 hours
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -243,7 +243,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
